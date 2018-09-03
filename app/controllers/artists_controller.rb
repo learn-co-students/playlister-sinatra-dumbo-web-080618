8de74fc4582f518f21481e58dfd99967483d5250
github username <github email address>
2018-09-03 17:43:54 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,24 @@
 class ArtistsController < ApplicationController
+
+get "/artists" do #reading the artists
+  @read_all = Artist.all
+  erb :"artists/index"
+end
+get "/artists/:slug" do
+  @artist = Artist.find_by_slug(params[:slug])
+  erb :"artists/show"
+end
+# get "/artists/new" do #loads new form
+#   @create = Artist.create(params)
+#   erb  :"artists/new"
+# end
+# post "/artists" do #creates a post
+#  @post = Artist.all
+#  erb :"artists/new"
+# end
+#
+#  get "/artists/:slug" do
+#    @art_slug = Artist.find_by_slug(params["slug"])
+#  end
+
 end
